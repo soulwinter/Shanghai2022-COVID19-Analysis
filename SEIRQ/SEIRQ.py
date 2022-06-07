0f918ef4865af68a0a5eb7fd1a04f2932fcab9b4
soulwinter <1127907836@qq.com>
2022-06-07 22:04:41 +0800

    最新版本

--- SEIRQ/SEIRQ.py
+++ SEIRQ/SEIRQ.py
@@ -1,76 +1,87 @@
 import random
 from math import floor
+from pandas import DataFrame
+import matplotlib.pyplot as plt
 
-timeLength = 400
 # 当发现疫情时，我们以一个防范区的范围为 S；即只考虑该防范区内的范围
-# TODO: 进入机制，因外地传入【考虑做核酸后直接放行】，发现病例时，需要做多次核酸检测
-# TODO: 康复概率
-# TODO: 随机机制
-
-S = [25000000] * timeLength  # 易感人群【位于普通区，没打疫苗】
-Sfk = [0] * timeLength  # 封控区人数
-Sgk = [0] * timeLength  # 管控区人数
-Sff = [0] * timeLength  # 防范区人数
-E = [0] * timeLength  # 潜伏期人群
-P = [0] * timeLength  # 确诊患者
-I = [0] * timeLength  # 无症状患者
-R = [0] * timeLength  # 康复人群（可能再次感染） TODO: 考虑怎么解决这件事，当前设定是不会再次感染
-D = [0] * timeLength  # 死亡人数
-Q = [0] * timeLength  # 隔离人群（居家隔离或方舱隔离）
-Qh = [0] * timeLength  # 医院隔离人数
 
 # 以下是可以调整的参数
-R0 = 8  # 直接感染率
-Rnormal = 5  # 防疫措施感染率
-Rfk = 0.05  # 封控区感染率（R * Rfk， 下同)
-Rjj = 0.2  # 居家隔离感染率（应该等于封控区）
+timeLength = 365  # 观测时长
+# 病毒特性
+R0 = 10  # 直接感染率
+deathRate = 35/100000  # 死亡率
+vaccineProtectionRate = 0.3  # 疫苗防护率
+asymptomaticRate = 0.9  # 无症状比例
+infectedAgainRate = 0.01
+lengthOfIncubationPeriod = 10  # 潜伏期长度
+lengthOfTreatment = 14  # 治疗时间
+asymptomaticToDiagnosedRate = 0.001  # 每天无症状向确诊转变的概率，注意和 asymptomaticRate 的关系
+# 政策措施
+frequencyOfTesting = 3  # 核酸检测频率
+useLockdown = True  # 发现病例时是否封控
+fkProportion = 0.4  # 封控区占比
+gkProportion = 0.4
+ffProportion = 0.2  # 用上面两个减去，程序中没有用到
+fkLeastTime = 7  # 连续多少天没病例就可以解除封控
+vaccineRate = 0.7  # 疫苗接种比率
+# 其他
+Rfk = 0.05  # 封控区感染率是普通感染率的比重（实际感染率 R0 * Rfk， 下同)
 Rgk = 0.5  # 管控区感染率
 Rff = 0.7  # 防范区感染率
-
-fkProportion = 0.5  # 封控区占比
-gkProportion = 0.2
-ffProportion = 0.3  # 用上面两个减去，程序中没有用到
-fkLeastTime = 7  # 连续多少天没病例就可以解除封控
-
-vaccineProtectionRate = 0.4  # 疫苗防护率
-vaccineRate = 0.7  # 疫苗接种比率
-asymptomaticRate = 0.9  # 无症状比例
 testValidityRate = 0.73  # 核酸检测有效率
-InfectedAgainRate = 0.1  # 再次被感染的概率
-
-lengthOfIncubationPeriod = 10  # 潜伏期长度
 lengthToHospital = 2  # 确诊患者从发病到去医院的时长
-frequencyOfTesting = 5  # 核酸检测频率
-lengthOfTreatment = 14  # 治疗时间
 
-# 初始化参数
+firstInfected = 100  # 最开始的无症状感染者
+totalHealthyPeople = 100000
+
+# ----------------------------------------------------------------------------------------------------
+
+# **以下初始化参数，请不要动**
+S = [totalHealthyPeople] * (timeLength + lengthOfTreatment * 3)  # 易感人群【位于普通区，没打疫苗】
+Sfk = [0] * (timeLength + lengthOfTreatment * 3)  # 封控区人数
+Sgk = [0] * (timeLength + lengthOfTreatment * 3)  # 管控区人数
+Sff = [0] * (timeLength + lengthOfTreatment * 3)  # 防范区人数
+E = [0] * (timeLength + lengthOfTreatment * 3)  # 潜伏期人群
+P = [0] * (timeLength + lengthOfTreatment * 3)  # 确诊患者
+I = [0] * (timeLength + lengthOfTreatment * 3)  # 无症状患者
+I[0] = firstInfected
+R = [0] * (timeLength + lengthOfTreatment * 3)  # 康复人群（可能再次感染）
+D = [0] * (timeLength + lengthOfTreatment * 3)  # 死亡人数
+Q = [0] * (timeLength + lengthOfTreatment * 3)  # 隔离人群（居家隔离或方舱隔离）
+Qh = [0] * (timeLength + lengthOfTreatment * 3)  # 医院隔离人数
+getI = [0] * (timeLength + lengthOfTreatment * 3)  # 医疗机构获得的无症状数据
+getP = [0] * (timeLength + lengthOfTreatment * 3)  # 医疗机构获得的确诊数据
+
 testDate = 0  # 核酸检测时间是否到 核酸检测频率
-toInfect = [0] * timeLength
-pToHospital = [0] * timeLength
-hospitalToR = [0] * timeLength
-qhToR = [0] * timeLength
-I[0] = 10000
+toInfect = [0] * (timeLength + lengthOfTreatment * 3)
+pToHospital = [0] * (timeLength + lengthOfTreatment * 3)
+hospitalToR = [0] * (timeLength + lengthOfTreatment * 3)
+qhToR = [0] * (timeLength + lengthOfTreatment * 3)
+qhToD = [0] * (timeLength + lengthOfTreatment * 3)
+qtoR = [0] * (timeLength + lengthOfTreatment * 3)
 noCasesDays = 0
-startLockdown = False
-# 模拟一年的动态清零
-for i in range(1, 365 + 1):
+inLockdown = False
+
+for i in range(1, timeLength + 1):
     # 初始化数据
     testDate += 1
-    lengthOfIncubationPeriod = random.randint(3, 15)
+    totalInitReinfected = random.randint(0, floor(R[i-1] * infectedAgainRate * 2))
 
     S[i] = S[i - 1]  # S 为所有可能感染的人群数量
     Sfk[i] = Sfk[i - 1]
     Sgk[i] = Sgk[i - 1]
     Sff[i] = Sff[i - 1]
     E[i] = E[i - 1]
-    totalS = S[i] + Sgk[i] + Sfk[i] + Sff[i]
     I[i] = I[i - 1]
     P[i] = P[i - 1]
-    Q[i] = Q[i-1]
-    Qh[i] = Qh[i-1]
+    Q[i] = Q[i - 1]
+    Qh[i] = Qh[i - 1]
+    R[i] = R[i - 1]
+    D[i] = D[i - 1]
+    totalS = S[i] + Sgk[i] + Sfk[i] + Sff[i] + totalInitReinfected  # 所有可以被感染的人数
 
     # 首先是感染进入潜伏期
-    totalInitInfected = floor((P[i - 1] + I[i - 1]) * R0 * (1-vaccineProtectionRate) * (1-vaccineRate))  # 如果没有任何措施，会感染的人数
+    totalInitInfected = floor((P[i] + I[i]) * R0 * (1 - vaccineProtectionRate) * (1 - vaccineRate))  # 如果没有任何措施，会感染的人数
     if totalS > 0:
         peopleInfectedFK = floor(totalInitInfected * Sfk[i] / totalS * Rfk)
         if peopleInfectedFK > Sfk[i]:
@@ -85,13 +96,50 @@ for i in range(1, 365 + 1):
         if peopleInfectedNormal > S[i]:
             peopleInfectedNormal = S[i]
         E[i] += peopleInfectedFF + peopleInfectedGK + peopleInfectedFK + peopleInfectedNormal
-        toInfect[i + lengthOfIncubationPeriod] += peopleInfectedFF + peopleInfectedGK + peopleInfectedFK + peopleInfectedNormal  # 在 i + lengthOfIncubationPeriod 需要转入确诊的人数
+        toInfect[
+            i + lengthOfIncubationPeriod] += peopleInfectedFF + peopleInfectedGK + peopleInfectedFK + peopleInfectedNormal  # 在 i + lengthOfIncubationPeriod 需要转入确诊的人数
         Sfk[i] -= peopleInfectedFK
         Sgk[i] -= peopleInfectedGK
         Sff[i] -= peopleInfectedFF
         S[i] -= peopleInfectedNormal
 
+        # 考虑再次感染
+        if totalInitReinfected > 0:
+            if inLockdown:
+                # 治愈者在封控区：
+                tempReinfected = floor(totalInitInfected * totalInitReinfected * fkProportion / totalS * Rfk)
+                if tempReinfected > floor(totalInitReinfected * fkProportion):
+                    tempReinfected = floor(totalInitReinfected * fkProportion)
+                toInfect[i+lengthOfIncubationPeriod] += tempReinfected
+                E[i] += tempReinfected
+                R[i] -= tempReinfected
+                # 治愈者在管控区
+                tempReinfected = floor(totalInitInfected * totalInitReinfected * gkProportion / totalS * Rgk)
+                if tempReinfected > floor(totalInitReinfected * gkProportion):
+                    tempReinfected = floor(totalInitReinfected * gkProportion)
+                toInfect[i + lengthOfIncubationPeriod] += tempReinfected
+                E[i] += tempReinfected
+                R[i] -= tempReinfected
+                # 治愈者在防范区
+                tempReinfected = floor(totalInitInfected * totalInitReinfected * ffProportion / totalS * Rff)
+                if tempReinfected > floor(totalInitReinfected * ffProportion):
+                    tempReinfected = floor(totalInitReinfected * ffProportion)
+                toInfect[i + lengthOfIncubationPeriod] += tempReinfected
+                E[i] += tempReinfected
+                R[i] -= tempReinfected
+            else:
+                tempReinfected = floor(totalInitInfected * totalInitReinfected / totalS)
+                if tempReinfected > floor(totalInitReinfected * ffProportion):
+                    tempReinfected = floor(totalInitReinfected * ffProportion)
+                toInfect[i + lengthOfIncubationPeriod] += tempReinfected
+                E[i] += tempReinfected
+                R[i] -= tempReinfected
 
+    # 无症状向确诊转变
+    if floor(I[i] * asymptomaticToDiagnosedRate) > 0:
+        P[i] += floor(I[i] * asymptomaticToDiagnosedRate)
+        I[i] -= floor(I[i] * asymptomaticToDiagnosedRate)
+        pToHospital[i + lengthToHospital] += floor(I[i] * asymptomaticToDiagnosedRate)
 
     # 潜伏期转入确诊或无症状
     if toInfect[i] > 0:
@@ -101,62 +149,105 @@ for i in range(1, 365 + 1):
         pToHospital[i + lengthToHospital] += toInfect[i] - floor(toInfect[i] * asymptomaticRate)
 
     print("------------------------------------------------------")
-    print(i, "可感染人数:", S[i] + Sfk[i] + Sgk[i] + Sff[i], "确诊病例:", P[i], "无症状：", I[i], "潜伏期：", E[i], "医院人数：", Qh[i], "方舱医院人数：", Q[i])
+    print(i, "可感染人数:", S[i] + Sfk[i] + Sgk[i] + Sff[i], "确诊病例:", P[i], "无症状：", I[i], "潜伏期：", E[i], "医院人数：", Qh[i],
+          "方舱医院人数：", Q[i], "治愈：", R[i], "死亡：", D[i])
 
     # 确诊病例去医院
     if pToHospital[i] > 0:
         Qh[i] += pToHospital[i]
         P[i] -= pToHospital[i]
-        qhToR[i + lengthOfTreatment] += pToHospital[i]
+        qhToD[i + random.randint(1, lengthOfTreatment)] += floor(pToHospital[i] * deathRate)
+        qhToR[i + lengthOfTreatment] += pToHospital[i] - floor(pToHospital[i] * deathRate)
+        getP[i] += pToHospital[i]  # 医疗机构获得
         print("第", i, "天: 新增确诊病例", pToHospital[i], "例.")
         # TODO: 政策响应
-        if not startLockdown:
+        if not inLockdown and useLockdown:
             print("开始封控")
-            startLockdown = True
+            inLockdown = True
             Sfk[i] = floor(S[i] * fkProportion)
             Sgk[i] = floor(S[i] * gkProportion)
             Sff[i] = S[i] - Sfk[i] - Sgk[i]
             S[i] = 0
 
+    if qhToD[i] > 0:
+        Qh[i] -= qhToD[i]
+        D[i] += qhToD[i]
+        print("第", i, "天: 新增死亡病例", round(qhToD[i]), "例.")
 
     if qhToR[i] > 0:
         Qh[i] -= qhToR[i]
         R[i] += qhToR[i]
         print("第", i, "天: 治愈确诊病例", round(qhToR[i]), "例.")
 
+    if qtoR[i] > 0:
+        Q[i] -= qtoR[i]
+        R[i] += qtoR[i]
+        print("第", i, "天: 无症状感染者解除隔离", round(qtoR[i]), "例.")
+
     # 全域规模的核酸检测
-    if testDate % frequencyOfTesting == 0 or startLockdown:
-        getPositive = 0
-        for j in range(0, I[i]):
-            if random.random() < testValidityRate:
+    if testDate % frequencyOfTesting == 0 or inLockdown:
+        # getPositive = 0
+        # for j in range(0, I[i]):
+        #     if random.random() < testValidityRate:
+        #         getPositive += 1
+        getPositive = floor(I[i] * testValidityRate)
+        if getPositive == I[i] - 1:
+            temp = random.random()
+            if temp < testValidityRate:
                 getPositive += 1
 
         I[i] -= getPositive
         Q[i] += getPositive
+        qtoR[i + lengthOfTreatment] += getPositive
 
-        if startLockdown and getPositive == 0:
+        if inLockdown and getPositive == 0:
             noCasesDays += 1
             if noCasesDays >= fkLeastTime:
-                startLockdown = False
+                inLockdown = False
                 S[i] = Sgk[i] + Sfk[i] + Sff[i]
                 Sgk[i] = 0
                 Sfk[i] = 0
                 Sff[i] = 0
                 noCasesDays = 0
-                print("封控解除!")
 
-        if startLockdown and getPositive != 0:
+        if inLockdown and getPositive != 0:
             noCasesDays = 0
 
-        if round(getPositive) > 0 or startLockdown:
+        if round(getPositive) > 0 or inLockdown:
             print("第", i, "天: 在全域核酸检测中，发现无症状感染者", round(getPositive), "例.")
+            getI[i] += getPositive  # 医疗机构获得
 
-        if not startLockdown and getPositive > 0:
+        if not inLockdown and getPositive > 0 and useLockdown:
             print("开始封控")
-            startLockdown = True
+            inLockdown = True
             Sfk[i] = floor(S[i] * fkProportion)
             Sgk[i] = floor(S[i] * gkProportion)
             Sff[i] = S[i] - Sfk[i] - Sgk[i]
             S[i] = 0
 
+# ----------------------------------------------------------------------------------------------------
 
+data = {
+    "实际确诊人数": P,
+    "实际无症状人数": I,  # 请注意，每天实际的无症状是减去了当日发现的病例的
+    "检测确诊人数": getP,
+    "检测无症状人数": getI,
+    "医院人数": Qh,
+    "方舱医院人数": Q,
+    "康复人数": R,
+    "死亡人数": D,
+    "未感染普通区人数": S,
+    "未感染封控区人数": Sfk,
+    "未感染管控区人数": Sgk,
+    "未感染防范区人数": Sff
+}
+# dataFrame 即最后返回结果，包含以上信息
+dataFrame = DataFrame(data)
+dataFrame[:timeLength].to_csv("result.csv")  # 保存 .csv 文件
+
+# 绘图，下图是一个简单样例，不是最终版本
+dayPlot = timeLength
+
+plt.plot(range(dayPlot), I[0:dayPlot], color="r")
+plt.plot(range(dayPlot), P[0:dayPlot], color="grey")
+plt.show()
